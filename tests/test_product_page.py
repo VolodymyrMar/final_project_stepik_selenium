1153c980dd26dd23e35764b48fa9190d70bd0ea9
27Marikkybinets <kybamar2012@gmail.com>
2020-09-24 22:28:58 +0300

    Add test_product_page and product_page

---
+++ tests/test_product_page.py
@@ -0,0 +1,12 @@
+from ..pages.product_page import ProductPage
+import time
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    assert page.compare_product_name_in_basket(), "Wrong name product added to basket"
+    assert page.compare_product_price_in_basket(), "Wrong price product added to basket"
+
